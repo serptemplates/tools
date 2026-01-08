54a0711bb626cec6a7d6e9505a74d4241c655083
devinschumacher <devin@serp.co>
2026-01-08 21:52:19 +0900

    Allow yt-dlp env vars in lint

--- apps/tools/eslint.config.js
+++ apps/tools/eslint.config.js
@@ -1,4 +1,18 @@
 import { nextJsConfig } from "@serp-tools/eslint-config/next-js";
 
+const turboEnvAllowList = [
+  "^YTDLP_BINARY_URL$",
+  "^YOUTUBE_DL_HOST$",
+  "^GITHUB_TOKEN$",
+  "^GH_TOKEN$",
+];
+
 /** @type {import("eslint").Linter.Config} */
-export default nextJsConfig;
+export default [
+  ...nextJsConfig,
+  {
+    rules: {
+      "turbo/no-undeclared-env-vars": ["warn", { allowList: turboEnvAllowList }],
+    },
+  },
+];
